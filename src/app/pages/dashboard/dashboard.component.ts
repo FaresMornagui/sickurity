5b0b0a14c077d122092e54741af23d29a91dee78
Sami Cherif <sami.chrif@lookiimobile.com>
2019-05-15 12:43:16 +0100

    fix

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { UserService } from '../core';
 
 @Component({
@@ -9,6 +9,15 @@ import { UserService } from '../core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
+  modalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) {}
+ 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
   ngOnInit() {
     
   }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardAuthResolver } from './dashboard-auth-resolver.service';
 import { SharedModule } from '../shared';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
   imports: [
    SharedModule,
+   ModalModule.forRoot(),
     DashboardRoutingModule
   ],
   declarations: [
